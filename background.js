a75b1769701cb9899501653a60d21d653a6a7110
SonGokuKakarrot <140271409+SonGokuKakarrot@users.noreply.github.com>
2026-09-11 15:46:39 -0600

    Update installation log message and badge color setting

--- background.js
+++ background.js
@@ -2,9 +2,7 @@
 
 function updateBadge(enabled) {
   chrome.action.setBadgeText({ text: enabled ? 'ON' : 'OFF' });
-  chrome.action.setBadgeBackgroundColor({
-    color: enabled ? '#00E676' : '#3A3A3A'
-  });
+  chrome.action.setBadgeBackgroundColor({ color: enabled ? '#00E676' : '#3A3A3A' });
 }
 
 chrome.storage.sync.get({ enabled: true }, function (stored) {
@@ -26,6 +24,6 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 
 chrome.runtime.onInstalled.addListener(function (details) {
   if (details.reason === 'install') {
-    console.log('Omni Meta Lord installed. Mic boost active on Messenger, Facebook, Instagram web calls.');
+    console.log('Omni Meta Lord v2 installed. Extreme mic boost active on Messenger, Facebook, Instagram web calls.');
   }
 });
