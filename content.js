cd0b97a84c0c312a360c257d3cad8735b097ca69
SonGokuKakarrot <140271409+SonGokuKakarrot@users.noreply.github.com>
2026-09-12 04:31:33 -0600

    Refactor message posting and theme fetching logic

--- content.js
+++ content.js
@@ -5,17 +5,41 @@
 (function () {
   'use strict';
 
+  const themeAsset = 'assets/363bc7ce3c45ce75bd795bd0ab88d936.gif';
+
+  function postToPage(type, payload) {
+    window.postMessage({
+      source: "Omni-Universal-Lord",
+      type,
+      ...payload
+    }, "*");
+  }
+
+  function sendTheme() {
+    try {
+      var url = chrome.runtime.getURL(themeAsset);
+      fetch(url, { method: 'HEAD' })
+        .then(function (res) {
+          if (res.ok) postToPage("OMNI_THEME", { themeUrl: url });
+        })
+        .catch(function () {});
+    } catch (e) {}
+  }
+
+  sendTheme();
+  window.addEventListener("message", function (event) {
+    if (event.source === window && event.data?.source === "Omni-Universal-Lord" && event.data.type === "OMNI_INJECTOR_READY") {
+      sendTheme();
+    }
+  });
+
   chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.type === 'omni-settings') {
-      window.postMessage({
-        source: "Omni-Universal-Lord",
-        type: "OMNI_CONFIG",
-        config: msg.settings
-      }, "*");
+      postToPage("OMNI_CONFIG", { config: msg.settings });
       sendResponse({ ok: true });
     }
     if (msg.type === 'omni-ping') {
-      window.postMessage({ source: "Omni-Universal-Lord", type: "OMNI_PING" }, "*");
+      postToPage("OMNI_PING", {});
       sendResponse({ ok: true });
     }
     return true;
